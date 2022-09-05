cd74b4a3f27039ae4452ada2c12aed56febaa57b
Shanatnu Raut <rshantanu73@gmail.com>
2022-09-06 01:34:19 +0530

    Update on Binary search

---
+++ Binarysearch/src/com/shantanu/Ceiling.java
@@ -0,0 +1,39 @@
+package com.shantanu;
+//If target is not available in array then print the gretest number of target.
+//basically ceiling means smallest number greater than <= eual to target.
+public class Ceiling {
+    public static void main(String[] args) {
+        int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
+        int target = 99;
+        int ans = ceiling(arr, target);
+        System.out.println(ans);
+    }
+
+    // return the index
+    // return -1 if it does not exist
+
+//    if target is grater than array then retun no ceiling
+    static int ceiling(int[] arr, int target) {
+        if(target > arr[arr.length-1]){
+            return -1;
+        }
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end) {
+            // find the middle element
+//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                // ans found
+                return mid;
+            }
+        }
+        return start;
+    }
+}
---
+++ Binarysearch/src/com/shantanu/Floor.java
@@ -0,0 +1,33 @@
+package com.shantanu;
+//Basically floor means gretest number <= target
+public class Floor {
+    public static void main(String[] args) {
+            int[] arr = {2, 3, 4, 15, 16, 18, 22, 45, 89};
+            int target = 1;
+            int ans = floor(arr, target);
+            System.out.println(ans);
+        }
+
+        // return the index
+        // return -1 if it does not exis t
+        static int floor(int[] arr, int target) {
+            int start = 0;
+            int end = arr.length - 1;
+
+            while(start <= end) {
+                // find the middle element
+//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
+                int mid = start + (end - start) / 2;
+
+                if (target < arr[mid]) {
+                    end = mid - 1;
+                } else if (target > arr[mid]) {
+                    start = mid + 1;
+                } else {
+                    // ans found
+                    return mid;
+                }
+            }
+            return end;
+        }
+    }
