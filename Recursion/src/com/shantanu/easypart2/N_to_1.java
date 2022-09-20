b85ee1eefe9003b7e048a680aff37541f6f82158
Shanatnu Raut <rshantanu73@gmail.com>
2022-09-20 23:42:28 +0530

    Day 2

---
+++ Recursion/src/com/shantanu/easypart2/N_to_1.java
@@ -0,0 +1,43 @@
+package com.shantanu.easypart2;
+
+public class N_to_1 {
+        public static void main(String[] args) {
+            funBoth(5);
+        }
+
+        // concept
+        static void concept(int n) {
+            if (n == 0) {
+                return;
+            }
+            System.out.println(n);
+//        concept(n--);
+            concept(--n);
+//        n-- vs --n
+        }
+
+        static void fun(int n) {
+            if (n == 0) {
+                return;
+            }
+            System.out.println(n);
+            fun(n-1);
+        }
+
+        static void funRev(int n) {
+            if (n == 0) {
+                return;
+            }
+            funRev(n-1);
+            System.out.println(n);
+        }
+
+        static void funBoth(int n) {
+            if (n == 0) {
+                return;
+            }
+            System.out.println(n);
+            funBoth(n-1);
+            System.out.println(n);
+        }
+    }
