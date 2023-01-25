2e7d7458bafea44a088b449bbb0fd4ba2412c663
s4shanatnu <rshantanu73@gmail.com>
2023-01-25 23:38:11 +0530

    Update Multidimentinalarr.java

--- Arrays/src/com/shantanu/Multidimentinalarr.java
+++ Arrays/src/com/shantanu/Multidimentinalarr.java
@@ -13,11 +13,7 @@ public class Multidimentinalarr {
         Scanner in = new Scanner(System.in);
 //        int[][] arr = new int[3][];
 
-//        int[][] arr = {
-//                {1, 2, 3}, // 0th index
-//                {4, 5}, // 1st index
-//                {6, 7, 8, 9} // 2nd index -> arr[2] = {6, 7, 8, 9}
-//        };
+//        
 
         int[][] arr = new int[3][3];
         System.out.println(arr.length); // no of rows
