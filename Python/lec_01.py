da82daf07a459350e639d08e9134371ca44fab40
Shantanu Raut <rshantanu73@gmail.com>
2023-09-12 15:31:56 +0530

    Dsa using python

---
+++ Python/lec_01.py
@@ -0,0 +1,27 @@
+print("Shantanu, Welcome to Python World!")     # String    # String is immutable
+print(2**4)           # Power of 2 to 4
+print(5//2)           # Floor Division
+print(5/2)            # Division    
+list=[1,2,3,34,4,5,66,77,88,99]             # List  # List is mutable
+print(list)
+tuple=(1,2,3,34,4,5,66,77,88,99)          # Tuple   # Tuple is immutable
+print(tuple)
+set={1,2,3,34,4,5,66,77,88,99}       # Set   # Set is mutable
+print(set)
+dict={1:"Shantanu",2:"Saurabh",3:"Sachin",4:"Sagar",5:"Sahil"}          # Dictionary    # Dictionary is mutable
+print(dict)
+
+
+# write a for loop to print 1 to 10
+for i in range(1,11):
+    print(i)
+
+# palindrome
+a=input("Enter a string: ")
+b=a[::-1]
+if a==b:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
+
+    
