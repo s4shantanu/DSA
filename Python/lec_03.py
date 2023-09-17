c141118d46ac093297f9f99f7c0802d776d01551
Shantanu Raut <rshantanu73@gmail.com>
2023-09-18 01:31:25 +0530

    update

--- Python/lec_03.py
+++ Python/lec_03.py
@@ -1,18 +1,38 @@
 #Pattern Examples 
 
-def pattern(n):
-    for i in range(n):
-        for j in range(i+1):
-            print("1", end=" ")
-        print("\r")
+# def pattern(n):
+#     for i in range(n):
+#         for j in range(i+1):
+#             print("1", end=" ")
+#         print("\r")
+# pattern(5)
 
-pattern(5)
+# def pattern2(n):
+#     for i in range(n):
+#         for j in range(n-i):
+#             print("1", end=" ")
+#         print("\r")
 
+# pattern2(5)
 
-def pattern2(n):
-    for i in range(n):
-        for j in range(n-i):
-            print("1", end=" ")
-        print("\r")
+#fibonacci seqence
+# 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610
 
-pattern2(5)
+# Fibonacci problems
+# def fib(n):
+#     if n==0:
+#         return 0
+#     elif n==1:
+#         return 1
+#     else:
+#         return (fib(n-1)+fib(n-2))
+# print(fib(10))
+
+def Fib(n):
+    n1, n2 = 0, 1
+    for i in range(2, n):
+        n3 = n1 + n2
+        n1 = n2
+        n2 = n3
+        print(n3, end=" ")
+Fib(10)
