c2734b141e015fac17d84a2ba3acd3af29d81bb7
Shantanu Raut <rshantanu73@gmail.com>
2024-06-01 12:18:53 +0530

    all pattern problems in 1 file

--- Python/pattern.py
+++ Python/pattern.py
@@ -4,4 +4,69 @@ def pattern(n):
         for j in range(n):
             print(j+1, end = "")
         print()
-pattern(n)
+# pattern(n)
+
+def pattern1(n):
+    for i in range(n):
+        for j in range(i):
+            print("*", end = "")
+        print()
+# pattern1(n)
+
+def nTriangle(n:int) ->None:
+    for i in range(n):
+        for j in range(i+1):
+            print(j+1,end=" ")
+        print()
+    # Write your solution here.
+    pass
+# nTriangle(n)
+
+def nNumberTriangle(n: int) -> None:
+    for i in range(n):
+        for j in range(n-i):
+            print(j+1, end=" ")
+        print()
+    # Write your solution here.
+    pass
+# nNumberTriangle(n)
+
+def nStarTriangle(n: int) -> None:
+    for i in range(n):
+        for j in range(i):
+            print(" ",end="")
+        for j in range(2*(n-1-i)+1):
+            print("*", end="")
+        print()
+    # Write your code here.
+    pass
+# nStarTriangle(n)
+
+
+def nStarTriangle1(n: int) -> None:
+    for i in range(n):
+        for j in range(n-i):
+            print(" ",end="")
+        for j in range(2*i+1):
+            print("*", end="")
+        print()
+    # Write your code here.
+    pass
+# nStarTriangle1(n)
+
+def nStarTriangle2(n: int) -> None:
+    for i in range(n):
+        for j in range(i):
+            print(" ",end="")
+        for j in range(2*(n-1-i)+1):
+            print("*", end="")
+        print()
+    for i in range(n):
+        for j in range(n-i):
+            print(" ",end="")
+        for j in range(2*i+1):
+            print("*", end="")
+        print()
+    # Write your code here.
+    pass
+# nStarTriangle2(n)
