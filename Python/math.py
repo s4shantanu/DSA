cb9ffd894ff1e8a261ae43a54fe02a5ca6d34589
Shantanu Raut <rshantanu73@gmail.com>
2024-06-02 18:39:16 +0530

    maths file update

--- Python/math.py
+++ Python/math.py
@@ -74,10 +74,10 @@
 
 
 #print divisors
-n = int(input("Enter the number: "))
-for i in range(1,n+1):
-    if n % i == 0:
-        print(i,end=" ")
-print()
+# n = int(input("Enter the number: "))
+# for i in range(1,n+1):
+#     if n % i == 0:
+#         print(i,end=" ")
+# print()
 
 
