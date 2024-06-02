d4995094a798cbb3b3d5397a18a5fc83ee41a561
Shantanu Raut <rshantanu73@gmail.com>
2024-06-02 11:50:23 +0530

    #

--- Python/math.py
+++ Python/math.py
@@ -1,5 +1,41 @@
 #  count degits problem 
 # enter a list input 
-lst = input("Enter the list: ")
-a = str(len(lst))
-print(a)
+# lst = input("Enter the list: ")
+# a = str(len(lst))
+# print(a)
+
+
+#reverse the number
+# n = int(input("Enter the number: "))
+# rev = 0
+# while n > 0:
+#     rem = n % 10
+#     rev = rev * 10 + rem
+#     n = n // 10
+# print(rev)
+
+
+
+#paliandrome number
+# n = int(input("Enter the number: "))
+# temp = n
+# rev = 0
+# while n > 0:
+#     rem = n % 10
+#     rev = rev * 10 + rem
+#     n = n // 10
+# if temp == rev:
+#     print("Paliandrome")
+# else:
+#     print("Not Paliandrome")
+
+
+
+#paliandrome string
+# s = input("Enter the string: ")
+# if s == s[::-1]:
+#     print("Paliandrome")
+# else:
+#     print("Not Paliandrome")
+
+
