b24de0fc1f4b99c908b06242d527f6c83eda76da
Shantanu Raut <rshantanu73@gmail.com>
2024-06-10 00:51:56 +0530

    #

--- Python/pattern.py
+++ Python/pattern.py
@@ -11,81 +11,81 @@ def pattern1(n):
         for j in range(i):
             print("*", end = "")
         print()
-# pattern1(n)
+pattern1(n)
 
-def nTriangle(n:int) ->None:
-    for i in range(n):
-        for j in range(i+1):
-            print(j+1,end=" ")
-        print()
-    # Write your solution here.
-    pass
-# nTriangle(n)
+# def nTriangle(n:int) ->None:
+#     for i in range(n):
+#         for j in range(i+1):
+#             print(j+1,end=" ")
+#         print()
+#     # Write your solution here.
+#     pass
+# # nTriangle(n)
 
-def nNumberTriangle(n: int) -> None:
-    for i in range(n):
-        for j in range(n-i):
-            print(j+1, end=" ")
-        print()
-    # Write your solution here.
-    pass
-# nNumberTriangle(n)
+# def nNumberTriangle(n: int) -> None:
+#     for i in range(n):
+#         for j in range(n-i):
+#             print(j+1, end=" ")
+#         print()
+#     # Write your solution here.
+#     pass
+# # nNumberTriangle(n)
 
-def nStarTriangle(n: int) -> None:
-    for i in range(n):
-        for j in range(i):
-            print(" ",end="")
-        for j in range(2*(n-1-i)+1):
-            print("*", end="")
-        print()
-    # Write your code here.
-    pass
-# nStarTriangle(n)
-
-
-def nStarTriangle1(n: int) -> None:
-    for i in range(n):
-        for j in range(n-i):
-            print(" ",end="")
-        for j in range(2*i+1):
-            print("*", end="")
-        print()
-    # Write your code here.
-    pass
-# nStarTriangle1(n)
-
-def nStarTriangle2(n: int) -> None:
-    for i in range(n):
-        for j in range(i):
-            print(" ",end="")
-        for j in range(2*(n-1-i)+1):
-            print("*", end="")
-        print()
-    for i in range(n):
-        for j in range(n-i):
-            print(" ",end="")
-        for j in range(2*i+1):
-            print("*", end="")
-        print()
-    # Write your code here.
-    pass
-# nStarTriangle2(n)
+# def nStarTriangle(n: int) -> None:
+#     for i in range(n):
+#         for j in range(i):
+#             print(" ",end="")
+#         for j in range(2*(n-1-i)+1):
+#             print("*", end="")
+#         print()
+#     # Write your code here.
+#     pass
+# # nStarTriangle(n)
 
 
-def nStarTriangle3(n: int) -> None:
-    for i in range(n):
-        for j in range(n-i):
-            print(" ",end="")
-        for j in range(2*i+1):
-            print("*", end="")
-        print()
-    for i in range(n):
-        for j in range(i):
-            print(" ",end="")
-        for j in range(2*(n-1-i)+1):
-            print("*", end="")
-        print()
-    # Write your code here.
-    pass
-# nStarTriangle3(n)
+# def nStarTriangle1(n: int) -> None:
+#     for i in range(n):
+#         for j in range(n-i):
+#             print(" ",end="")
+#         for j in range(2*i+1):
+#             print("*", end="")
+#         print()
+#     # Write your code here.
+#     pass
+# # nStarTriangle1(n)
+
+# def nStarTriangle2(n: int) -> None:
+#     for i in range(n):
+#         for j in range(i):
+#             print(" ",end="")
+#         for j in range(2*(n-1-i)+1):
+#             print("*", end="")
+#         print()
+#     for i in range(n):
+#         for j in range(n-i):
+#             print(" ",end="")
+#         for j in range(2*i+1):
+#             print("*", end="")
+#         print()
+#     # Write your code here.
+#     pass
+# # nStarTriangle2(n)
+
+
+# def nStarTriangle3(n: int) -> None:
+#     for i in range(n):
+#         for j in range(n-i):
+#             print(" ",end="")
+#         for j in range(2*i+1):
+#             print("*", end="")
+#         print()
+#     for i in range(n):
+#         for j in range(i):
+#             print(" ",end="")
+#         for j in range(2*(n-1-i)+1):
+#             print("*", end="")
+#         print()
+#     # Write your code here.
+#     pass
+# # nStarTriangle3(n)
 
---
+++ Recursion/recursion.py
@@ -0,0 +1,9 @@
+#recursion
+def name(n):
+    if n == 0:
+        return
+    print()
+    name(n+1)
+
+
+name(5,14)
